d35a5abd05b3daa6d724bf98f88688a2b4c02597
Dongwan Kim <zeumes12@gmail.com>
2019-03-27 11:38:17 -0400

    Added services and contact pages - completed restructured to Angular

--- client/src/app/pages/projects/projects.component.ts
+++ client/src/app/pages/projects/projects.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+export class ProjectsComponent extends BasePageComponent implements OnInit {
+  constructor(route: ActivatedRoute) {
+    super(route);
+   }
 
   ngOnInit() {
   }
